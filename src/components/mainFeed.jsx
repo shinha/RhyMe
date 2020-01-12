6a4b6d879fa5d990adbb7c7f73820e586f1636ac
Shinha Tsuchida <stsuchida@Apriso-Colormasters-MacBook-Pro-2.local>
2020-01-12 13:10:37 -0800

    Improved UI styling

--- src/components/mainFeed.jsx
+++ src/components/mainFeed.jsx
@@ -13,7 +13,7 @@ export default class MainFeed extends Component{
             rhymeList3:[],
             slantRhymeList:[],
             ideaList:[],
-            isLoaded: false
+            stateLoaded: 0 // 1 = Synonym, 2 = Rhyme, 3 = Idea
         }
     }
 
@@ -22,12 +22,11 @@ export default class MainFeed extends Component{
         let keyWord = window.getSelection().toString()
         
         try{
-            let result = fetch("https://dictionaryapi.com/api/v3/references/thesaurus/json/" + keyWord + "?key=" + config.MERRIAM_WEBSTER_KEY)
+            fetch("https://dictionaryapi.com/api/v3/references/thesaurus/json/" + keyWord + "?key=" + config.MERRIAM_WEBSTER_KEY)
                 .then(res => res.json())
                 .then(json => {
-                    console.log(JSON.stringify(json))
                     this.setState({
-                        isLoaded: true,
+                        stateLoaded: 1,
                         synonymList: json[0].meta.syns.flat()
                     })
                 })
@@ -36,33 +35,6 @@ export default class MainFeed extends Component{
         }
     }
 
-    formatRhymeData(jsonObj){
-        // Note: each item in the json object is a separate word
-        let rhyme1 = []
-        let rhyme2 = []
-        let rhyme3 = []
-
-        for (let ele in jsonObj){
-            let numSyllables = jsonObj[ele].numSyllables
-
-            // Add word to respective list based on the # of syllables
-            switch(numSyllables){
-                case 1:
-                    rhyme1.append(jsonObj[ele].word)
-                    break
-                case 2:
-                    rhyme2.append(jsonObj[ele].word)
-                    break
-                case 3:
-                    rhyme3.append(jsonObj[ele].word)
-                    break
-                default:
-                    // Disregard any words above 4-syllables
-                    break
-            }
-        } 
-        return [rhyme1, rhyme2, rhyme3]
-    }
     
     handleGetRhyme = () =>{
         let keyword = window.getSelection().toString();
@@ -73,7 +45,7 @@ export default class MainFeed extends Component{
             .then(json=>{
                 let cleanJson = this.formatRhymeData(json)
                 this.setState({
-                    isLoaded:true,
+                    stateLoaded: 2,
                     rhymeList1: cleanJson[0],
                     rhymeList2: cleanJson[1],
                     rhymeList3: cleanJson[2],
@@ -84,14 +56,67 @@ export default class MainFeed extends Component{
             console.log("ERROR: " + e)
         }
     }
+    formatRhymeData(jsonObj){
+        // Note: each item in the json object is a separate word
+        let rhyme1 = []
+        let rhyme2 = []
+        let rhyme3 = []
+
+        for (let ele in jsonObj){
+            let numSyllables = jsonObj[ele].numSyllables
+            // Add word to respective list based on the # of syllables
+            switch(numSyllables){
+                case 1:
+                    rhyme1.push(jsonObj[ele].word)
+                    break
+                case 2:
+                    rhyme2.push(jsonObj[ele].word)
+                    break
+                case 3:
+                    rhyme3.push(jsonObj[ele].word)
+                    break
+                default:
+                    // Disregard any words above 4-syllables
+                    break
+            }
+        } 
+        
+        return [rhyme1, rhyme2, rhyme3]
+    }
 
     render(){
         const notepad = {
             padding: '8px',
-            margin: '8px'
+            margin: '8px',
+            backgroundImage: 'url("https://image.freepik.com/free-photo/design-space-paper-textured-background_53876-42312.jpg")',
+            backgroundSize: 'cover',
+            fontSize: '16px',
+            width:'80%',
+            height:'800px',
         }
 
-        var {isLoaded, synonymList} = this.state
+        var {stateLoaded, synonymList, rhymeList1, rhymeList2, rhymeList3} = this.state
+
+        let resultPanel = <p></p>
+
+        let lyrics = "Yeah, I'm gonna take my horse to the old town road\nI'm gonna ride 'til I can't no more\nI'm gonna take my horse to the old town road\nI'm gonna ride 'til I can't no more (Kio, Kio)\nI got the horses in the back\nHorse tack is attached\nHat is matte black\nGot the boots that's black to match\nRidin' on a horse, ha\nYou can whip your Porsche\nI been in the valley\nYou ain't been up off that porch, now"
+        console.log(rhymeList2)
+
+        switch(stateLoaded){
+            case 1:
+                // Synonyms panel
+                resultPanel = synonymList.map(listItem => <li>{listItem}</li>)
+                break
+            case 2:
+                // Rhyme panel
+                resultPanel = rhymeList2.map(listItem => <li>{listItem}</li>)
+                break    
+            case 3:
+                // TODO: implement this 
+                break
+            default:
+                break                    
+        }
 
         return(
         <div >
@@ -115,11 +140,11 @@ export default class MainFeed extends Component{
             </div>
             <div className="Step-1 row">
                 <div style={{background:'#ff9c9c'} } className="col-8">
-                    <textarea style={notepad} defaultValue="ok"></textarea>
+                    <textarea style={notepad} defaultValue={lyrics}></textarea>
                 </div>
                 <div style={{background:'#dd9c9c'}} className="col-4 row ">
                     <ul>
-                        {synonymList.map(listItem => <li>{listItem}</li>)}
+                        {resultPanel}
                     </ul>
                 </div>
             </div>
