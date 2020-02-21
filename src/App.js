3763aa0684c0013bb02e6856b90a7132cc75bc54
Shinha Tsuchida <stsuchida@Apriso-Colormasters-MacBook-Pro-2.local>
2020-02-20 21:01:17 -0800

    Updated RhyMe ReactApp Repo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import MainFeed from './components/mainFeed'
-import NavBar from './components/navBar'
 
 
 
@@ -10,18 +9,7 @@ function App() {
   return (
     <div className="App" style = {{width:'100%'}}>
       <header className="App-header">
-        <NavBar/>
         <MainFeed/>
-        <img className="App-logo" alt="logo" />
-        <h2><input/></h2>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/components/mainFeed.jsx
+++ src/components/mainFeed.jsx
@@ -1,154 +1,60 @@
 import React, {Component} from 'react'
+import ResultPanel from '../components/resultPanel'
 import 'bootstrap/dist/css/bootstrap.css'
-import {config} from '../config.js'
 
 export default class MainFeed extends Component{
 
     constructor(props){
         super(props);
         this.state = {
-            synonymList: [],
-            rhymeList1:[],
-            rhymeList2:[],
-            rhymeList3:[],
-            slantRhymeList:[],
-            ideaList:[],
+            selectedWord: "",
             stateLoaded: 0 // 1 = Synonym, 2 = Rhyme, 3 = Idea
         }
+        this.handleActionClick = this.handleActionClick.bind(this);
     }
-
-    handleGetSyn =()=> {
-
-        let keyWord = window.getSelection().toString()
-        
-        try{
-            fetch("https://dictionaryapi.com/api/v3/references/thesaurus/json/" + keyWord + "?key=" + config.MERRIAM_WEBSTER_KEY)
-                .then(res => res.json())
-                .then(json => {
-                    this.setState({
-                        stateLoaded: 1,
-                        synonymList: json[0].meta.syns.flat()
-                    })
-                })
-        }catch(e){
-            console.log("ERROR: " + e)
-        }
-    }
-
     
-    handleGetRhyme = () =>{
-        let keyword = window.getSelection().toString();
-        try{
-            // TODO: Make call for actual rhynmes
-            fetch("https://api.datamuse.com/words?rel_rhy=" + keyword)
-            .then(res => res.json())
-            .then(json=>{
-                let cleanJson = this.formatRhymeData(json)
-                this.setState({
-                    stateLoaded: 2,
-                    rhymeList1: cleanJson[0],
-                    rhymeList2: cleanJson[1],
-                    rhymeList3: cleanJson[2],
-                })
-            })
-            // TODO: Make call for near rhymes
-        }catch(e){
-            console.log("ERROR: " + e)
-        }
+    //Sets the stateLoaded, which determines the type of button action was selected
+    handleActionClick = (actionIndex) =>{
+        this.setState({
+            stateLoaded: actionIndex,
+            selectedWord: window.getSelection().toString()
+        });
     }
-    formatRhymeData(jsonObj){
-        // Note: each item in the json object is a separate word
-        let rhyme1 = []
-        let rhyme2 = []
-        let rhyme3 = []
 
-        for (let ele in jsonObj){
-            let numSyllables = jsonObj[ele].numSyllables
-            // Add word to respective list based on the # of syllables
-            switch(numSyllables){
-                case 1:
-                    rhyme1.push(jsonObj[ele].word)
-                    break
-                case 2:
-                    rhyme2.push(jsonObj[ele].word)
-                    break
-                case 3:
-                    rhyme3.push(jsonObj[ele].word)
-                    break
-                default:
-                    // Disregard any words above 4-syllables
-                    break
-            }
-        } 
-        
-        return [rhyme1, rhyme2, rhyme3]
-    }
 
     render(){
-        const notepad = {
-            padding: '8px',
-            margin: '8px',
-            backgroundImage: 'url("https://image.freepik.com/free-photo/design-space-paper-textured-background_53876-42312.jpg")',
-            backgroundSize: 'cover',
-            fontSize: '16px',
-            width:'80%',
-            height:'800px',
-        }
+        const {selectedWord, stateLoaded} = this.state
 
-        var {stateLoaded, synonymList, rhymeList1, rhymeList2, rhymeList3} = this.state
 
-        let resultPanel = <p></p>
-
-        let lyrics = "Yeah, I'm gonna take my horse to the old town road\nI'm gonna ride 'til I can't no more\nI'm gonna take my horse to the old town road\nI'm gonna ride 'til I can't no more (Kio, Kio)\nI got the horses in the back\nHorse tack is attached\nHat is matte black\nGot the boots that's black to match\nRidin' on a horse, ha\nYou can whip your Porsche\nI been in the valley\nYou ain't been up off that porch, now"
-        console.log(rhymeList2)
-
-        switch(stateLoaded){
-            case 1:
-                // Synonyms panel
-                resultPanel = synonymList.map(listItem => <li>{listItem}</li>)
-                break
-            case 2:
-                // Rhyme panel
-                resultPanel = rhymeList2.map(listItem => <li>{listItem}</li>)
-                break    
-            case 3:
-                // TODO: implement this 
-                break
-            default:
-                break                    
-        }
+        const lyrics = "Yeah, I'm gonna take my horse to the old town road\nI'm gonna ride 'til I can't no more\nI'm gonna take my horse to the old town road\nI'm gonna ride 'til I can't no more (Kio, Kio)\nI got the horses in the back\nHorse tack is attached\nHat is matte black\nGot the boots that's black to match\nRidin' on a horse, ha\nYou can whip your Porsche\nI been in the valley\nYou ain't been up off that porch, now"
+        
 
         return(
-        <div >
-            <h1>Yuh</h1>
-            <h2>Welcome to traphouse v1.0.</h2>
-            <div className="Step-1 row">
-                <div style={{background:'#ff9c9c'} } className="col-8">
-                    This is the left side
+            <div >
+                <h2 style={{padding:'8px'}}>RhyMe Lyric Editor v1.0.</h2>
+                <div className="Step-1 row">
+                    <div style={{background:'#ff9c9c',color:'black'} } className="p-3 col-8">
+                        {this.state.selectedWord === "" ? "Select a word to highlight to use synonym / rhyme query." : "You selected:" + this.state.selectedWord}
+                    
+                    </div>
+                    <div style={{background:'#dd9c9c'}} className="p-3 col-4 row">
+                        <div className="btn1 col-sm">
+                            <button type="button" onClick={() => this.handleActionClick(1)} className="p-2 btn btn-synonym btn-dark">Synonym</button>
+                        </div>
+                        <div className="btn1 col-sm">
+                            <button type="button" onClick={() => this.handleActionClick(2)} className="p-2 btn btn-rhyme btn-dark">Rhyme</button>
+                        </div>
+                    </div>
                 </div>
-                <div style={{background:'#dd9c9c'}} className="col-4 row">
-                    <div className="btn1 col-sm">
-                        <button type="button" onClick={this.handleGetSyn} className="btn btn-synonym btn-primary">Synonym</button>
+                <div className="Step-1 row">
+                    <div style={{background:'#ff9c9c'} } className="col-8">
+                        <textarea defaultValue={lyrics}></textarea>
                     </div>
-                    <div className="btn1 col-sm">
-                        <button type="button" onClick={this.handleGetRhyme} className="btn btn-rhyme btn-primary">Rhyme</button>
-                    </div>
-                    <div className="btn1 col-sm">
-                        <button type="button" className="btn btn-idea btn-primary">Idea</button>
+                    <div style={{background:'#dd9c9c'}} className="col-4 row ">
+                        <ResultPanel selectedWord = {selectedWord} stateLoaded={stateLoaded}/>
                     </div>
                 </div>
             </div>
-            <div className="Step-1 row">
-                <div style={{background:'#ff9c9c'} } className="col-8">
-                    <textarea style={notepad} defaultValue={lyrics}></textarea>
-                </div>
-                <div style={{background:'#dd9c9c'}} className="col-4 row ">
-                    <ul>
-                        {resultPanel}
-                    </ul>
-                </div>
-            </div>
-        </div>
         )
     }
 }
